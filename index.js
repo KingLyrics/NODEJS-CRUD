a6731b65b70968a5f1de89552df06f41cc872de3
KingLyrics <ekom338.com@gmail.com>
2023-05-03 09:32:19 +0100

    GET REQUEST AND POST ADDED.POST request is currently giving issues as the GET is working

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const mysql = require("mysql");
+const http = require("http");
+const JSON = require('body-parser')
 require("dotenv").config();
 
 const conn = mysql.createConnection({
@@ -15,4 +17,52 @@ conn.connect((err) => {
   }
 
   console.log("Connected to the DB");
+});
+
+const server = http.createServer((req, res) => {
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  const sendResponse = (statusCode, payload) => {
+    res.writeHead(statusCode, headers);
+    res.end(JSON.stringify(payload));
+  };
+
+  if (req.method === "GET" && req.url === "/employees") {
+    conn.query("SELECT * FROM Employee_data", (error, resuts) => {
+      if (error) {
+        sendResponse(500, { error: "Error fetching employees" });
+      } else {
+        sendResponse(200, resuts);
+        console.log("Here is the list of all users");
+      }
+    });
+  } else if (req.method === "POST" && req.url === "/employees") {
+
+    const { name, age, phone, years_of_exp, department, skills } = body;
+
+    conn.query(
+      `INSERT INTO Employee_data (name,age,phone,yearsofexp,department,skills) VALUES ('${name}', ${age}, ${phone}, ${years_of_exp}, '${department}', '${skills}')`,
+      (error, results) => {
+        if (error) {
+          sendResponse(500, { error: "Error adding employees" });
+        } else {
+          sendResponse(201, results);
+          console.log("Employee Added to the database");
+        }
+      }
+    );
+  } else if (req.method === "DELETE" && req.url === "/about:id") {
+    // Handle all other requests
+  } else if (req.method === "UPDATE" && req.url === "/employee:id") {
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end("404 Not Found");
+  }
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Running on ${process.env.PORT}`);
 });
